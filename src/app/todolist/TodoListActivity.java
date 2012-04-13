11758b937b00ab882b1774edddb07925de673b5b
nttai88 <nttai88@gmail.com>
2012-04-13 14:43:38 +0700

    init

---
+++ src/app/todolist/TodoListActivity.java
@@ -0,0 +1,40 @@
+package app.todolist;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class TodoListActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		ListView listView = (ListView) findViewById(R.id.myListView);
+		final EditText editText = (EditText) findViewById(R.id.myEditText);
+		final ArrayList<String> todoItems = new ArrayList<String>();
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+		listView.setAdapter(arrayAdapter);
+		editText.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == KeyEvent.ACTION_DOWN)
+					if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+						
+						EditText edittxt = (EditText)v;
+						
+						todoItems.add(0, edittxt.getText().toString());
+						arrayAdapter.notifyDataSetChanged();
+						edittxt.setText("");
+						return true;
+					}
+				return false;
+			}
+		});
+	}
+}
